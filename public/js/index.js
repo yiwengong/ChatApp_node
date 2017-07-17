1dfafea019a1e989e04fe15ef084d00f7e497bcf
Yiwen Gong <gongyiwen2017@gmail.com>
2017-07-16 23:27:29 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server.');
-
-  socket.emit('createMessage', {
-    from: 'test@test.com',
-    text:'Hey, this is test.'
-  });
 });
 
 socket.on('disconnect', function() {
