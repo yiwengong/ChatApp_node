a960233eb2eb9c0dbf58591b0b871e1d159f88b7
Yiwen Gong <gongyiwen2017@gmail.com>
2017-07-17 15:01:55 -0400

    Add user class and updateUserList event

--- server/utils/users.js
+++ server/utils/users.js
@@ -11,12 +11,12 @@ class Users {
 
   removeUser (id) {
     var user = this.getUser(id);
-    if(user) {
-      this.users = this.users.filter((user)=> user.id !== id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
     }
 
     return user;
-
   }
 
   getUser(id) {
